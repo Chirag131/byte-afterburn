3b82494d71b368e9a74add094e1ff81d75994e23
Chirag131 <chiragtuteja2006@gmail.com>
2026-09-15 00:02:37 +0530

    Changed description

--- src/components/home/WhoAreWe.tsx
+++ src/components/home/WhoAreWe.tsx
@@ -128,10 +128,10 @@ export default function WhoAreWe() {
           </div>
 
           <div className="whoarewe-bottom">
-            <h3 className="whoarewe-role">BUILD. CONNECT.<br />CREATE & IMPACT.</h3>
+            <h3 className="whoarewe-role">WE BUILD<br />REAL TECH.</h3>
             <div className="whoarewe-description">
-              <h3>STUDENT-LED<br />TECH SOCIETY</h3>
-              <p>B.Y.T.E. is a community of innovators, thinkers, and doers. We organize events, build projects, and collaborate to create meaningful impact.</p>
+              <h3>CIRCUITS.<br />CODE. SYSTEMS.</h3>
+              <p>Student engineers solving problems with hardware, software, and everything between. No theory. Just builds.</p>
             </div>
           </div>
 
